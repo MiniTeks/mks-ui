f6896fefdd2ce000d71e3cd30135f1b56a0f6597
Avinal Kumar <avinkuma@redhat.com>
2022-02-12 22:52:52 +0530

    Add documentation to the mks-ui
    - Add docs to the functions
    - update readme
    
    Signed-off-by: Avinal Kumar <avinkuma@redhat.com>

--- pkg/db/dbQuery.go
+++ pkg/db/dbQuery.go
@@ -1,7 +1,25 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2022 Satyam Bhardwaj <sabhardw@redhat.com>
+// SPDX-FileCopyrightText: 2022 Utkarsh Chaurasia <uchauras@redhat.com>
+// SPDX-FileCopyrightText: 2022 Avinal Kumar <avinkuma@redhat.com>
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//    http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package db
 
 import "github.com/go-redis/redis/v8"
 
+// GetValue returns the value of a given key from the redis database.
 func GetValue(rClient *redis.Client, key string) string {
 	val, err := rClient.Get(ctx, key).Result()
 	if err != nil {
@@ -9,9 +27,11 @@ func GetValue(rClient *redis.Client, key string) string {
 	}
 	return val
 }
+
+// GetValues returns the keys in the Application struct using the GetValue function.
 func GetValues(rClient *redis.Client) (*Application, error) {
-	dataPoint := []string{"created", "deleted", "active", "failed", "completed"}
-	resources := []string{"mksTask", "mksTaskRun", "mksPipelineRun"}
+	dataPoint := []string{"CREATED", "DELETED", "ACTIVE", "FAILED", "COMPLETED"}
+	resources := []string{"MKSTASK", "MKSTASKRUN", "MKSPIPELINERUN"}
 	ans := [3]Mksresource{}
 	for j, r := range resources {
 		resArr := [5]string{}
